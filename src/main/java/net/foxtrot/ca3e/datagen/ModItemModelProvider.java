52e8cd572595ff274c6d4783266787e8e126d1b1
rero78 <54547757+rero78@users.noreply.github.com>
2025-12-31 15:36:50 -0600

    Generate item models via datagen

--- src/main/java/net/foxtrot/ca3e/datagen/ModItemModelProvider.java
+++ src/main/java/net/foxtrot/ca3e/datagen/ModItemModelProvider.java
@@ -17,7 +17,30 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-
+        simpleItem(ModItems.ANDESITE_MECHANISM);
+        simpleItem(ModItems.INCOMPLETE_ANDESITE_MECHANISM);
+        simpleItem(ModItems.ANDESITE_SHEET);
+        simpleItem(ModItems.PREPARED_ANDESITE_MECHANISM);
+        simpleItem(ModItems.PNEUMATIC_MECHANISM);
+        simpleItem(ModItems.INCOMPLETE_PNEUMATIC_MECHANISM);
+        simpleItem(ModItems.STERILE_MECHANISM);
+        simpleItem(ModItems.INCOMPLETE_STERILE_MECHANISM);
+        simpleItem(ModItems.SILVER_SHEET);
+        simpleItem(ModItems.INDUSTRIAL_MECHANISM);
+        simpleItem(ModItems.INCOMPLETE_INDUSTRIAL_MECHANISM);
+        simpleItem(ModItems.ZINC_SHEET);
+        simpleItem(ModItems.COMPUTATION_MECHANISM);
+        simpleItem(ModItems.INCOMPLETE_COMPUTATION_MECHANISM);
+        simpleItem(ModItems.INVAR_SHEET);
+        simpleItem(ModItems.ADVANCED_COMPUTATION_MECHANISM);
+        simpleItem(ModItems.INCOMPLETE_ADVANCED_COMPUTATION_MECHANISM);
+        simpleItem(ModItems.SIGNALUM_SHEET);
+        simpleItem(ModItems.ULTRA_LIGHTWEIGHT_MECHANISM);
+        simpleItem(ModItems.INCOMPLETE_ULTRA_LIGHTWEIGHT_MECHANISM);
+        simpleItem(ModItems.ALUMINUM_SHEET);
+        simpleItem(ModItems.HEAVY_DUTY_MECHANISM);
+        simpleItem(ModItems.INCOMPLETE_HEAVY_DUTY_MECHANISM);
+        simpleItem(ModItems.NETHERITE_SHEET);
     }
 
     private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
